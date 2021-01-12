e36e7ff813a5f9a69e09402ec9fb688059f7008c
Cliff <duartejhfaria@gmail.com>
2021-01-12 02:58:20 +0000

    arrow no masks

--- src/components/Masks.vue
+++ src/components/Masks.vue
@@ -6,7 +6,7 @@
         </div>
     </div>
     <div v-else>
-        <span>no masks</span>
+        <a href="#" class="arrow up">Up</a>
     </div>
 </template>
 
@@ -32,10 +32,46 @@
 </script>
 
 <style lang="scss" scoped>
-    .masks {
-        display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-        grid-gap: 1rem;
-        margin-left: 2%;
-    }
+  @import "./../styles/_variables.scss";
+
+  .masks {
+      display: grid;
+      grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+      grid-gap: 1rem;
+      margin-left: 2%;
+  }
+
+  @keyframes uparrow {
+      0% {
+          -webkit-transform: translateY(0);
+          opacity: 0.4
+      }
+
+      100% {
+          -webkit-transform: translateY(-0.4em);
+          opacity: 0.9
+      }
+  }
+
+
+  .arrow {
+      border-color: transparent;
+      border-style: solid;
+      border-width: 0 2em;
+      display: block;
+      height: 0;
+      margin: 1em auto;
+      left: 170px;
+      opacity: 0.4;
+      text-indent: -9999px;
+      transform-origin: 50% 50%;
+      width: 0;
+      position: absolute;
+  }
+
+  .up {
+      -webkit-animation: uparrow 0.6s infinite alternate ease-in-out;
+      border-bottom: 2em solid $hp-blue;
+  }
+
 </style>
